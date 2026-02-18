f9a37fbae4803eec63cc54be7d705482333516b7
ViweNdlala <viwendlala806@gmail.com>
2026-02-18 11:12:56 +0200

    Declaring a winner section done

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -12,17 +12,39 @@ function Square({value, onSquareClick}){
   );
 }
 export default function Board(){
+  const [xIsNext, setXIsNext] = useState(true);   // Used to keep information of which player goes next
   const [squares, setSquares] = useState(Array(9).fill(null));
-
 
   // handleClick: function to update the value of square with o,x,null
   function handleClick(i){
+    // avoid move overwriting another one
+    if(squares[i] || calculateWinner(squares)){   //determine if there is already a value in position of square
+      return;
+    }
     const nextSquares = squares.slice();  // Copy of the squares array
-    nextSquares[i] = "X";
+    // handle the players taking turns
+    if(xIsNext){
+      nextSquares[i] = "X";
+    }else{
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+
   }
+
+  // Determine the winner
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner){
+    status = "Winner" + winner;
+  }else{
+    status = "Next Player: "+ (xIsNext ? "X" : "O");
+  }
+  
   return (
     <>
+      <div className="status">{status}</div>
       <div className="border-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -40,4 +62,25 @@ export default function Board(){
       </div>
     </>
   );
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for(let i =0; i<lines.length; i++){
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
